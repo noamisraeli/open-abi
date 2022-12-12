a8052c9659e60976cb247d47be427d54f254f704
noami <noamisraeli97@gmail.com>
2022-12-12 17:50:03 +0200

    Make contract client not knowning web3

--- openabi/python/contract_client.py
+++ openabi/python/contract_client.py
@@ -5,31 +5,34 @@ import pydantic
 import web3
 from web3.contract import Contract
 
-Bytes32 = bytes
 Address = pydantic.constr(regex=r'^0x[0-9a-fA-F]{40}$')
 
-
-class ContractClient:
-    ABI: str 
-
-    def __init__(self, address: Address, w3_provider: web3.Web3) -> None:
-        assert self.ABI
-        self._address = address
-        self._web3_provider = w3_provider
-        self._contract_api: typing.Optional[Contract] = None
+class EtheruemGatewayAPI:
+    def __init__(self, w3: web3.Web3) -> None:
+        self._web3_provider = w3
     
-    def init(self):
-        self._contract_api = self._web3_provider.eth.contract(self._address, abi=self.ABI)
-    
-    def make_call(self, function_signature: str, arguments: typing.List):
+    def make_call(self, address: Address, function_signature: str, arguments: typing.List) -> typing.Any:
         data = encode(function_signature, args=arguments)
         output = self._web3_provider.eth.call({
-            'to': self._address,
+            'to': address,
             'data': data
         })
         return decode(function_signature, output)
-        
 
-    
 
+TGatewayAPI = typing.TypeVar('TGatewayAPI')
+
+class ContractClient(typing.Generic[TGatewayAPI]):
+    ABI: str 
+
+    def __init__(self, address: Address, gateway_api: TGatewayAPI) -> None:
+        assert self.ABI
+        self._address = address
+        self._gateway_api = gateway_api
     
+    def init(self):
+        pass
+
+    @property
+    def api(self) -> TGatewayAPI:
+        return self._gateway_api
